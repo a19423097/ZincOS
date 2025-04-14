ae8cba7b1bf58d8cc76dad5aa65c8eba2faf7219
a19423097 <a19423097@gmail.com>
2025-04-14 15:02:50 +0200

    Add files via upload

--- zOS/v0.1.0.1.5/src/ZincOS/ZincOS.ino
+++ zOS/v0.1.0.1.5/src/ZincOS/ZincOS.ino
@@ -1,5 +1,16 @@
-#include "Mkernel.h"
-#include "Trimmer.h"
+#if __has_include("Mkernel.h")
+  #include "Mkernel.h"
+  #define HAS_MKERNEL
+#else
+  #warning "Mkernel.h not found. Mkern features will be disabled."
+#endif
+
+#if __has_include("Trimmer.h")
+  #include "Trimmer.h"
+  #define HAS_TRIMMER
+#else
+  #warning "Trimmer.h not found. Trimmer features will be disabled."
+#endif
 
 const int MAX_LINES = 30;
 String lines[MAX_LINES];
@@ -17,7 +28,22 @@ delay1000$
 char code2[200] = "";
 
 void linesInterpret(String code, char* mem1) {
+#ifdef HAS_TRIMMER
   Trimmer::splitAndTrimLines(code.c_str(), lines, lineCount, MAX_LINES);
+#else
+  // Fallback: simple newline splitter
+  lineCount = 0;
+  int lastIndex = 0;
+  for (int i = 0; i < code.length(); i++) {
+    if (code[i] == '\n' && lineCount < MAX_LINES) {
+      lines[lineCount++] = code.substring(lastIndex, i);
+      lastIndex = i + 1;
+    }
+  }
+  if (lastIndex < code.length() && lineCount < MAX_LINES) {
+    lines[lineCount++] = code.substring(lastIndex);
+  }
+#endif
 
   int i = 0;
   while (i < lineCount) {
@@ -48,11 +74,9 @@ void linesInterpret(String code, char* mem1) {
   }
 }
 
-
-
 void interpret(String b, char* mem) {
   if (b.startsWith("pr")) {
-    char str[51]; // max 50 characters + null terminator
+    char str[51];
     int i = 0;
 
     if (b[2] == 'm' && b[3] == '*') {
@@ -64,7 +88,7 @@ void interpret(String b, char* mem) {
       str[i] = b[i + 2];
       i++;
     }
-    str[i] = '\0'; // null-terminate the string
+    str[i] = '\0';
     Serial.println(str);
 
   } else if (b.startsWith("sav")) {
@@ -79,8 +103,8 @@ void interpret(String b, char* mem) {
   } else if (b.startsWith("clrmem")) {
     for (int i = 0; i < 50; i++) {
       mem[i] = '\0';
-  }
-  Serial.println("memory cleared.");
+    }
+    Serial.println("memory cleared.");
 
   } else if (b.startsWith("dwr")) {
     int i = 0;
@@ -88,7 +112,7 @@ void interpret(String b, char* mem) {
     while (b[i + 3] != '$' && i < 3) {
       wpinStr[i] = b[i + 3];
       i++;
-  }
+    }
     wpinStr[i] = '\0';
 
     char wvalStr[2];
@@ -99,8 +123,11 @@ void interpret(String b, char* mem) {
     int wval = String(wvalStr).toInt();
 
     pinMode(wpin, OUTPUT);
+#ifdef HAS_MKERNEL
     Mkern::dwr(wpin, wval);
-
+#else
+    Serial.println("Mkern::dwr not available. Skipping pin write.");
+#endif
     Serial.println("set pin " + String(wpinStr) + " to " + wvalStr);
 
   } else if (b.startsWith("drd")) {
@@ -113,11 +140,15 @@ void interpret(String b, char* mem) {
     rpinStr[i] = '\0';
 
     int rpin = String(rpinStr).toInt();
-
     pinMode(rpin, INPUT);
-    int readval = Mkern::drd(rpin);
 
-    Serial.println("read from pin " + String(rpinStr) + " and got value " + readval);
+#ifdef HAS_MKERNEL
+    int readval = Mkern::drd(rpin);
+#else
+    int readval = -1;
+    Serial.println("Mkern::drd not available. Using dummy value.");
+#endif
+    Serial.println("read from pin " + String(rpinStr) + " and got value " + String(readval));
 
   } else if (b.startsWith("run")) {
     if (b[3] == '1') {
@@ -131,42 +162,41 @@ void interpret(String b, char* mem) {
   } else if (b.startsWith("delay")) {
     char time[10];
     int i = 0;
-
-    while (b[i + 5] != '$' && i < 9) { // also guard against overflow
+    while (b[i + 5] != '$' && i < 9) {
       time[i] = b[i + 5];
       i++;
     }
     time[i] = '\0';
     int delayms = atoi(time);
     delay(delayms);
-  
-} else if (b.startsWith("append")) {
-  int i = 0, j = 0;
-  char str[51];
-  while (b[i + 6] != '@' && j < 50) {
-    char ch = b[i + 6];
-    if (ch == '\\' && b[i + 7] == 'n') {
-      str[j++] = '\n';
-      i += 2; // skip the '\n' characters
-    } else {
-      str[j++] = ch;
-      i++;
-    }
-  }
-  str[j] = '\0'; // null-terminate the string
 
-  if (b[b.length() - 1] == '1') {
-    Serial.println("read only storage.");
-  } else if (b[b.length() - 1] == '2') {
-    code2[sizeof(code2) - 1] = '\0';
-    strcat(code2, str);
-    Serial.println("appended \"" + String(str) + "\" into 2");
-  }
-} else {
+  } else if (b.startsWith("append")) {
+    int i = 0, j = 0;
+    char str[51];
+    while (b[i + 6] != '@' && j < 50) {
+      char ch = b[i + 6];
+      if (ch == '\\' && b[i + 7] == 'n') {
+        str[j++] = '\n';
+        i += 2;
+      } else {
+        str[j++] = ch;
+        i++;
+      }
+    }
+    str[j] = '\0';
+
+    if (b[b.length() - 1] == '1') {
+      Serial.println("read only storage.");
+    } else if (b[b.length() - 1] == '2') {
+      code2[sizeof(code2) - 1] = '\0';
+      strcat(code2, str);
+      Serial.println("appended \"" + String(str) + "\" into 2");
+    }
+
+  } else {
     Serial.println("command " + String(b) + " was not found.");
   }
 }
-
 
 void setup() {
   Serial.begin(9600);
@@ -181,7 +211,7 @@ void loop() {
 
     if (readByte == '\n') {
       Serial.println(buffer);
-      interpret(buffer, memory); // pass buffer and memory to interpret
+      interpret(buffer, memory);
       Serial.print("OK|");
       buffer = "";
     } else {
