1f8301c54f1af18b87fedc936bf576d4db63a623
a19423097 <a19423097@gmail.com>
2025-04-15 11:45:50 +0200

    Update Mkernel.h

--- zOS/v0.1.0.1.6/src/ZincOS/Mkernel.h
+++ zOS/v0.1.0.1.6/src/ZincOS/Mkernel.h
@@ -1,4 +1,9 @@
 struct Mkern {
+  static bool ignorePanic;
+  static void setup(bool ignoreKernelPanic) {
+    ignorePanic = ignoreKernelPanic;
+  }
+
   static void dwr(int pin, int val) {
     digitalWrite(pin, val);
   }
@@ -8,7 +13,27 @@ struct Mkern {
   }
 
   static void clrmem(char* mem, int size) {
+    const int memSize = 50;
     for (int i = 0; i < size; i++) {
+      if (i > memSize) {
+        Serial.println("KERNEL_PANIC: UNKOWN DATA MIGHT HAVE BEEN OVERWRITTEN/INVALID MEMORY SIZE");
+        Serial.println("THE SYSTEM HAS TO STOP TO AVOID CRASHES");
+        Serial.print("reseting in:");
+        Serial.print("3");
+        delay(1000);
+        Serial.print("2");
+        delay(1000);
+        Serial.println("1");
+        dwr(13, 0);
+        delay(2000);
+        if (ignorePanic) {
+          Serial.println("ignorePanic was true. returning to ZincShell");
+          return;
+        } else {
+          Serial.print("it does not look like the system has reset. connect pin 13 to RESET if your using an arduino Uno.");
+          while(1);
+        }
+      }
       mem[i] = '\0';
     }
   }
@@ -21,4 +46,4 @@ struct Mkern {
     }
     mem[i] = '\0';
   }
-};
+};         
